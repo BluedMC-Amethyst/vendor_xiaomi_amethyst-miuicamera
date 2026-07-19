ee1eeca7bb15d563634999152569414fe0d4ffd9
Bluekyeet <bluekyeet@bluedmc.xyz>
2026-07-19 21:58:53 +1200

    amethyst: Inital Commit (patched apk)

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+#MiuiCamera.apk
+
+cat vendor/xiaomi/amethyst-miuicamera/proprietary/system/priv-app/MiuiCamera/MiuiCamera.part_* > vendor/xiaomi/amethyst-miuicamera/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
