f90abb23033881f34aed48f5f88fd19221b8adc5
Bluekyeet <bluekyeet@bluedmc.xyz>
2026-09-23 20:52:28 +1200

    amthyst: miuicamera: vendorsetup.sh: Add check for existing MiuiCamera.apk before concatenating parts

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
-#MiuiCamera.apk
+# Check if MiuiCamera.apk exists, if not, concatenate the parts
 
+[ -f vendor/xiaomi/amethyst-miuicamera/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk ] || \
 cat vendor/xiaomi/amethyst-miuicamera/proprietary/system/priv-app/MiuiCamera/MiuiCamera.part_* > vendor/xiaomi/amethyst-miuicamera/proprietary/system/priv-app/MiuiCamera/MiuiCamera.apk
